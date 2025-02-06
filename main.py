21f3f6604a868096abf78fc04fd3cda054dd1386
dweavp <100860878+pashatett@users.noreply.github.com>
2025-02-06 18:56:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 def add_everything_up(a, b):
     try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            result = a + b
-        elif isinstance(a, (int, float)) and isinstance(b, str):
-            result = str(a) + b
-        elif isinstance(a, str) and isinstance(b, (int, float)):
-            result = a + str(b)
-        else:
-            raise TypeError("Типы данных несовместимы для сложения.")
+        result = a + b
+        return result
+    
     except TypeError:
-        result = f"{a} + {b}"
-    return result
-
+        return f"{a}{b}"
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
-print(add_everything_up("123", "123"))
+print(add_everything_up(123.456, 7)) 
